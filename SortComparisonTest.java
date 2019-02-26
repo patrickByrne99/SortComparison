da764e0298b5761392731a1956f82704f9215f05
patrickByrne99 <47981108+patrickByrne99@users.noreply.github.com>
2019-02-26 11:33:01 +0000

    Update SortComparisonTest.java

--- SortComparisonTest.java
+++ SortComparisonTest.java
@@ -44,6 +44,112 @@ public class SortComparisonTest
 		testArray = SortComparison.selectionSort(testArray); 
 		assertEquals("Selection empty test",0, testArray.length);
     }
+	
+    @Test
+    public void testInsertionSort() {
+    	
+    	double pos[] = {222.7, 24, 13, 47.2, 0, 1.9, 26, 115.5, 218.4, 218};
+    	double posSorted[] = {0, 1.9, 13, 24, 26, 47.2, 115.5, 218, 218.4, 222.7};
+    	assertArrayEquals(SortComparison.insertionSort(pos),posSorted,0);
+    	
+    	double neg[] = {-222.7, -24, -13, -47.2, 0, -1.9, -26, -115.5, -218.4, -218};
+    	double negSorted[] = {-222.7, -218.4, -218, -115.5, -47.2, -26, -24, -13, -1.9, 0};
+    	assertArrayEquals(SortComparison.insertionSort(neg),negSorted,0);
+    
+    double intDoub[] = {23.1, -49.4, 300, 4000, 369.6, 23, -116.6, 0, -342, -37};
+    	double intDoubSorted[] = {-342, -116.6, -49.4, -37, 0, 23, 23.1, 300, 369.6, 4000};
+    	assertArrayEquals(SortComparison.insertionSort(intDoub),intDoubSorted,0);
+    
+    	double back[] = {9,8,7,6,5,4,3,2,1,0};
+    	double backSorted[] = {0,1,2,3,4,5,6,7,8,9};
+    	assertArrayEquals(SortComparison.insertionSort(back),backSorted,0);
+    }
+
+    
+     
+
+	@Test 
+    public void testQuick() 
+    { 
+	double pos[] = {222.7, 24, 13, 47.2, 0, 1.9, 26, 115.5, 218.4, 218};
+    	double posSorted[] = {0, 1.9, 13, 24, 26, 47.2, 115.5, 218, 218.4, 222.7};
+    	assertArrayEquals(SortComparison.quickSort(pos),posSorted,0);
+    	
+    	double neg[] = {-222.7, -24, -13, -47.2, 0, -1.9, -26, -115.5, -218.4, -218};
+    	double negSorted[] = {-222.7, -218.4, -218, -115.5, -47.2, -26, -24, -13, -1.9, 0};
+    	assertArrayEquals(SortComparison.quickSort(neg),negSorted,0);
+    
+    double intDoub[] = {23.1, -49.4, 300, 4000, 369.6, 23, -116.6, 0, -342, -37};
+    	double intDoubSorted[] = {-342, -116.6, -49.4, -37, 0, 23, 23.1, 300, 369.6, 4000};
+    	assertArrayEquals(SortComparison.quickSort(intDoub),intDoubSorted,0);
+    
+    	double back[] = {9,8,7,6,5,4,3,2,1,0};
+    	double backSorted[] = {0,1,2,3,4,5,6,7,8,9};
+    	assertArrayEquals(SortComparison.quickSort(back),backSorted,0);
+    }
+    
+    @Test 
+    public void testMergeIterative() 
+    { 
+    	double pos[] = {222.7, 24, 13, 47.2, 0, 1.9, 26, 115.5, 218.4, 218};
+    	double posSorted[] = {0, 1.9, 13, 24, 26, 47.2, 115.5, 218, 218.4, 222.7};
+    	assertArrayEquals(SortComparison.mergeSortIterative(pos),posSorted,0);
+    	
+    	double neg[] = {-222.7, -24, -13, -47.2, 0, -1.9, -26, -115.5, -218.4, -218};
+    	double negSorted[] = {-222.7, -218.4, -218, -115.5, -47.2, -26, -24, -13, -1.9, 0};
+    	assertArrayEquals(SortComparison.mergeSortIterative(neg),negSorted,0);
+    
+    double intDoub[] = {23.1, -49.4, 300, 4000, 369.6, 23, -116.6, 0, -342, -37};
+    	double intDoubSorted[] = {-342, -116.6, -49.4, -37, 0, 23, 23.1, 300, 369.6, 4000};
+    	assertArrayEquals(SortComparison.mergeSortIterative(intDoub),intDoubSorted,0);
+    
+    	double back[] = {9,8,7,6,5,4,3,2,1,0};
+    	double backSorted[] = {0,1,2,3,4,5,6,7,8,9};
+    	assertArrayEquals(SortComparison.mergeSortIterative(back),backSorted,0);
+    }
+    
+    @Test 
+    public void testMergeRecursive() 
+    { 
+    	double pos[] = {222.7, 24, 13, 47.2, 0, 1.9, 26, 115.5, 218.4, 218};
+    	double posSorted[] = {0, 1.9, 13, 24, 26, 47.2, 115.5, 218, 218.4, 222.7};
+    	assertArrayEquals(SortComparison.mergeSortRecursive(pos),posSorted,0);
+    	
+    	double neg[] = {-222.7, -24, -13, -47.2, 0, -1.9, -26, -115.5, -218.4, -218};
+    	double negSorted[] = {-222.7, -218.4, -218, -115.5, -47.2, -26, -24, -13, -1.9, 0};
+    	assertArrayEquals(SortComparison.mergeSortRecursive(neg),negSorted,0);
+    
+    double intDoub[] = {23.1, -49.4, 300, 4000, 369.6, 23, -116.6, 0, -342, -37};
+    	double intDoubSorted[] = {-342, -116.6, -49.4, -37, 0, 23, 23.1, 300, 369.6, 4000};
+    	assertArrayEquals(SortComparison.mergeSortRecursive(intDoub),intDoubSorted,0);
+    
+    	double back[] = {9,8,7,6,5,4,3,2,1,0};
+    	double backSorted[] = {0,1,2,3,4,5,6,7,8,9};
+    	assertArrayEquals(SortComparison.mergeSortRecursive(back),backSorted,0);
+    }
+
+    
+    @Test 
+    public void testSelection() 
+    { 
+    	double pos[] = {222.7, 24, 13, 47.2, 0, 1.9, 26, 115.5, 218.4, 218};
+    	double posSorted[] = {0, 1.9, 13, 24, 26, 47.2, 115.5, 218, 218.4, 222.7};
+    	assertArrayEquals(SortComparison.selectionSort(pos),posSorted,0);
+    	
+    	double neg[] = {-222.7, -24, -13, -47.2, 0, -1.9, -26, -115.5, -218.4, -218};
+    	double negSorted[] = {-222.7, -218.4, -218, -115.5, -47.2, -26, -24, -13, -1.9, 0};
+    	assertArrayEquals(SortComparison.selectionSort(neg),negSorted,0);
+    
+    double intDoub[] = {23.1, -49.4, 300, 4000, 369.6, 23, -116.6, 0, -342, -37};
+    	double intDoubSorted[] = {-342, -116.6, -49.4, -37, 0, 23, 23.1, 300, 369.6, 4000};
+    	assertArrayEquals(SortComparison.selectionSort(intDoub),intDoubSorted,0);
+    
+    	double back[] = {9,8,7,6,5,4,3,2,1,0};
+    	double backSorted[] = {0,1,2,3,4,5,6,7,8,9};
+    	assertArrayEquals(SortComparison.selectionSort(back),backSorted,0);
+    }
+	    
+		
 
 
     // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
